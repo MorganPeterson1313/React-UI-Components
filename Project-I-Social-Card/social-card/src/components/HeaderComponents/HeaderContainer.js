a1cd13e3542244c9ab53a0dcb9ab5409162cc50e
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-05-08 20:46:40 -0400

    initial commit completed Headercontent file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,40 @@
+
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+
+
+
+
+const HeaderContainer = () => {
+
+        return(
+                        <div className = "mainHeader">
+                        <ImageThumbnail />
+
+
+                                    <div className = "title-content">
+                                    <HeaderTitle/>
+                                    <HeaderContent/>
+                                    </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        ); 
+
+};
+
+export default HeaderContainer;
