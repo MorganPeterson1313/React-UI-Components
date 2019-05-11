8299b92201b20965631a9119ee20ed248295c9cc
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-05-10 23:35:43 -0400

    completed numberButton file initial commit

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,33 @@
 import React from 'react';
 import './Button.css';
+
+
+const numbers = [
+{number: "9"},
+{number: "8"},
+{number: "7"},
+{number: "6"},
+{number: "5"},
+{number: "4"},
+{number: "3"},
+{number: "2"},
+{number: "1"},
+{number: "0"}
+]
+
+
+
+function NumberButton(props) {
+    return (
+      <div>
+  
+  <button className={props.buttonStyle}>
+          {numbers.map(number => (
+            <NumberButton number={number} key={number.number} />
+          ))}
+        </button>
+      </div>
+    );
+  }
+  
+  export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+
+
+
+const CalculatorDisplay =  props => {
+return (
+
+        <div>
+
+        <NumberButton/>
+
+
+        </div>
+
+    );
+
+};
+
+export default CalculatorDisplay;
